a0b3e79b5cd1be426a4c386b7a00f28392249c86
Gavin Crowley <gavincrowley@gmail.com>
2024-03-28 21:42:01 +0000

    day 1

---
+++ Schuler/15_Days/day_1.sql
@@ -0,0 +1,13 @@
+select distinct district
+from address
+
+select rental_date
+from rental
+order by 1 desc
+limit 1
+
+select count(film_id)
+from film
+
+select count(distinct last_name)
+from customer
