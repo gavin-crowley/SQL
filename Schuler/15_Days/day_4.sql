6dcb2f235aff6e63cd4991e82a56d483b97b39d0
Gavin Crowley <gavincrowley@gmail.com>
2024-04-02 15:41:05 +0100

    day 4 complete

--- Schuler/15_Days/day_4.sql
+++ Schuler/15_Days/day_4.sql
@@ -8,3 +8,37 @@ select '***' || substring(email from (position('.' in email) - 1 ) for 1)
 || '.' || substring(email from (position('.' in email) + 1 ) for 1) || '***'
 || substring(email from (position('@' in email)))
 from customer
+
+
+select extract(dow from payment_date), sum(amount)
+from payment
+group by 1
+order by 2 desc
+
+
+select sum(amount), to_char(payment_date, 'Dy, DD/MM/YYYY')
+from payment
+group by 2
+order by 1 asc
+
+
+select sum(amount), to_char(payment_date, 'Mon, YYYY')
+from payment
+group by 2
+order by 1 asc
+
+select sum(amount), to_char(payment_date, 'Dy, HH:MI')
+from payment
+group by 2
+order by 1 desc
+
+select customer_id, return_date - rental_date 
+from rental
+where customer_id = 35
+order by 1 desc
+
+
+select customer_id, avg(return_date - rental_date) 
+from rental
+group by 1
+order by 2 desc
