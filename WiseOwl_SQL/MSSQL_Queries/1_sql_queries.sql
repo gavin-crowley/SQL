af2ca457ab066e18962153513c2221f893e38aa4
Gavin Crowley <gavincrowley@gmail.com>
2024-05-18 19:29:24 +0100

    may 18

--- WiseOwl_SQL/MSSQL_Queries/1_sql_queries.sql
+++ WiseOwl_SQL/MSSQL_Queries/1_sql_queries.sql
@@ -1,11 +1,26 @@
+--PART 1
+
 --This is a comment
 /*
 Created by Wise Owl
 Show a list of films
 */
-USE Movies
-SELECT 
-	[FilmName] AS Title
+--USE Movies
+--SELECT 
+--	[FilmName] AS Title
+--	,FilmReleaseDate [Released on]
+--	,FilmOscarWins
+--FROM dbo.tblFilm
+
+
+--PART 2
+--USE Movies
+
+SELECT [FilmName] AS Title
 	,FilmReleaseDate [Released on]
-	,FilmOscarWins
+	,FilmRunTimeMinutes/60 AS [Hours]
+	,FilmRunTimeMinutes%60 AS [Minutes]
 FROM dbo.tblFilm
+ORDER BY FilmOscarWins DESC
+
+
